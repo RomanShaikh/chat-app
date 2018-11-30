00955edec43f4c446431fcaf41e23ab8bbc6d827
RomanShaikh <romushaikh94@gmail.com>
2018-11-30 08:23:29 +0530

    Add greetings and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,12 @@
 var socket = io();
         socket.on('connect', function(){
             console.log('Connected to the server');
-
-            socket.emit('createEmail',{
-                to : 'romi@example.com',
-                text : 'Hey Roms ... How are you ?'
-            });
-
-            //Create Message
-
-            socket.emit('createMessage',{
-                to : 'roms@example.com',
-                text : 'Hello friends'
-            });
         });
 
         socket.on('newMessage', function(message){
-            console.log(message);
+            console.log('New Message ',message);
         });
 
         socket.on('disconnect', function(){
             console.log('Disconnected from the server');
-        });
-
-        socket.on('newEmail',function(email){
-            console.log('New Email',email);
         });
--- server/server.js
+++ server/server.js
@@ -10,32 +10,34 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connect',(socket)=>{
-    console.log('New user is connected')
+    console.log('New user is connected');
 
-    socket.emit('newEmail',{
-        from : 'Roman@example.com',
-        text : 'How are you ?'
+        socket.emit('newMessage',{
+            from : 'Admin',
+            text : 'Welcome to chat app'
+        });
+
+        socket.broadcast.emit('newMessage',{
+            from : 'Admin',
+            text : 'New User joined',
+            createdAt : new Date().getTime()
+        });
+
+        socket.on('createMessage',(message)=>{
+            
+            io.emit('newMessage',{
+                from : message.to,
+                text : message.text,
+                createdAt : new Date().getTime()
+            });
+        });
+        
+
+        socket.on('disconnect',()=>{
+            console.log('User disconnected');
+        });
     });
-
-    //CreateMessage
-    socket.emit('newMessage',{
-        from : 'Server.com',
-        text : 'Hellow from server',
-        createdAt : new Date().getDate()
-    });
-
-    socket.on('createEmail',(createEmail)=>{
-        console.log('Create Email : ',createEmail);
-    });
-
-    socket.on('createMessage',(newMessage)=>{
-        console.log('Create message: ',newMessage);
-    })
-
-    socket.on('disconnect',()=>{
-        console.log('User disconnected');
-    });
-});
+   
 
 server.listen(3000 , ()=>{
     console.log('Server is up on port 3000');
