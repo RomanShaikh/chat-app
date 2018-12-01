71992afd0312ecc788a187c242788361004b79ac
RomanShaikh <romushaikh94@gmail.com>
2018-12-01 12:11:51 +0530

    Add message file

--- server/server.js
+++ server/server.js
@@ -3,33 +3,24 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
+const { generateMessage } = require('./utils/messages');
 const publicPath = path.join(__dirname, '../public');
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
 io.on('connect',(socket)=>{
     console.log('New user is connected');
 
-        socket.emit('newMessage',{
-            from : 'Admin',
-            text : 'Welcome to chat app'
-        });
+        socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
 
-        socket.broadcast.emit('newMessage',{
-            from : 'Admin',
-            text : 'New User joined',
-            createdAt : new Date().getTime()
-        });
+        socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
         socket.on('createMessage',(message)=>{
             
-            io.emit('newMessage',{
-                from : message.to,
-                text : message.text,
-                createdAt : new Date().getTime()
-            });
+            io.emit('newMessage',generateMessage(message.from,message.text));
         });
         
 
