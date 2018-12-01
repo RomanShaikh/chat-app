4018be6cd507f0dde742f1f239e47795b476cfd6
RomanShaikh <romushaikh94@gmail.com>
2018-12-01 15:49:01 +0530

    Add input from user side

--- server/server.js
+++ server/server.js
@@ -18,12 +18,16 @@ io.on('connect',(socket)=>{
 
         socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 
-        socket.on('createMessage',(message)=>{
-            
-            io.emit('newMessage',generateMessage(message.from,message.text));
-        });
-        
-
+        socket.on('createMessage',(message, callback) => {
+            console.log('createMessage', message);
+            io.emit('newMessage', generateMessage(message.from, message.text));
+            callback('This is from the server.');
+            // socket.broadcast.emit('newMessage', {
+            //   from: message.from,
+            //   text: message.text,
+            //   createdAt: new Date().getTime()
+            // });
+          });
         socket.on('disconnect',()=>{
             console.log('User disconnected');
         });
